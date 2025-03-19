ada5125db9218a4ae0ded3383f39437858abd62a
marko dimitrijevic <Marko.Dimitrijevic2@Bosch.com>
2025-03-19 10:35:28 +0100

    Login added

--- DenLogin/ContentView.swift
+++ DenLogin/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel: LoginVM
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Token:")
+            Text(viewModel.token)
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(viewModel: LoginVM())
 }
+
+final class LoginVM: ObservableObject {
+    @Published var token = ""
+    let service = LoginService()
+    init() {
+        Task {
+            await provideToken()
+        }
+    }
+    
+    @MainActor
+    private func provideToken() async {
+        do {
+            token = try await service.getToken()
+        } catch {
+            print("error fetching token...")
+            print("\(error)")
+        }
+        
+    }
+}
+
+
---
+++ DenLogin/LoginService.swift
@@ -0,0 +1,58 @@
+//
+//  LoginService.swift
+//  DenLogin
+//
+//  Created by Marko Dimitrijevic on 19.3.25..
+//
+
+import Foundation
+
+protocol LoginServiceProtocol {
+    func getToken() async throws -> String
+}
+
+struct LoginService: LoginServiceProtocol {
+    
+    func getToken() async throws -> String {
+        let endpoint = "http://localhost:3000/login"
+        let url = URL(string: endpoint)! // Adjust the URL accordingly
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let requestBody: [String: Any] = [
+            "username": "robkeg99",
+            "password": "Roby1999"
+        ]
+        request.httpBody = try JSONSerialization.data(withJSONObject: requestBody, options: [])
+        
+        let (_, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+            throw URLError(.badServerResponse)
+        }
+
+        guard let cookie = httpResponse.allHeaderFields["Set-Cookie"] as? String else {
+            throw URLError(.badServerResponse)
+        }
+        
+        guard let sessionId = extractSessionToken(from: cookie) else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return sessionId
+    }
+    
+    private func extractSessionToken(from cookie: String) -> String? {
+        let pattern = #"acc-session=([a-f0-9\-]+)"#
+        
+        if let regex = try? NSRegularExpression(pattern: pattern, options: []),
+           let match = regex.firstMatch(in: cookie, options: [], range: NSRange(location: 0, length: cookie.utf16.count)) {
+            
+            if let range = Range(match.range(at: 1), in: cookie) {
+                return String(cookie[range])
+            }
+        }
+        
+        return nil
+    }
+}
