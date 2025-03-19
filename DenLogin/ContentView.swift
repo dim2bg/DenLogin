39a722747d455816294ccc0e2ba2c477ce61d637
marko dimitrijevic <Marko.Dimitrijevic2@Bosch.com>
2025-03-19 09:42:09 +0100

    Initial Commit

---
+++ DenLogin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DenLogin
+//
+//  Created by Marko Dimitrijevic on 19.3.25..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DenLogin/DenLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenLoginApp.swift
+//  DenLogin
+//
+//  Created by Marko Dimitrijevic on 19.3.25..
+//
+
+import SwiftUI
+
+@main
+struct DenLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
